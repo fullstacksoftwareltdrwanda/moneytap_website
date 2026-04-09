8859f763583489bdcd99136251bef7c69a368da0
ishimweghislain <ishimweghislain82@gmail.com>
2026-04-09 15:26:52 +0200

    Favicon

--- app.moneytap/index.php
+++ app.moneytap/index.php
@@ -239,8 +239,11 @@ p, .form-label, .small {
     <title>Moneytap | Financial Dashboard</title>
     
     <!-- Favicon -->
-    <link rel="icon" type="image/png" href="https://moneytap.rw/favicon.png">
-    <link rel="shortcut icon" href="https://moneytap.rw/favicon.png">
+    <link rel="icon" href="https://moneytap.rw/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="https://moneytap.rw/favicon.ico" type="image/x-icon">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://moneytap.rw/favicon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://moneytap.rw/favicon.png">
+    <link rel="apple-touch-icon" href="https://moneytap.rw/favicon.png">
     
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
